1d8819315ebf06ef1c6f0e3bebc867fc0969a670
albertgoico <42077327+AlbertGoico@users.noreply.github.com>
2021-12-21 00:22:11 -0500

    formateando la salida
    formateando la salida

--- Ejercicio01/hanoi.py
+++ Ejercicio01/hanoi.py
@@ -1,54 +1,81 @@
 from Pila import Pila
 
-print("La cantidad de discos debe ser mayor o igual a 3")
-cantidad = int(input("Ingrese el numero de discos: "))
+
+print("\n{:3}{:<5}  ".format("  ", "-"*50))
+print("{:15}{:<0}  ".format("  ", "TORRES DE HANOI"))
+print("{:3}{:<5}  ".format("  ", "-"*50))
+print("\n{:3}{:<5}  ".format("  ", "La cantidad de discos debe ser mayor o igual a 3" ))
+
+cantidad = int(input("\n{:3}{:<0}  ".format("  ", "Ingrese el numero de discos: ") ))
+
 
 TorreInicial = Pila()
 TorreMedio = Pila()
 TorreFinal = Pila()
 
+
 def Llenar_torreInicial():
     for i in range(cantidad):
-        disco = int(input("Numero del disco {}:".format(i+1)))
+        #disco = int(input())
+        disco = int(input("\n{:3}{:<0}  ".format("  ","Numero del disco {}:".format(i + 1))))
+
         TorreInicial.items.append(disco)
 
-def Llenar_torreFinalconInicial():    
+
+def Llenar_torreFinalconInicial():
     disco_retirar = TorreInicial.pop()
     TorreFinal.items.append(disco_retirar)
+
 
 def Llenar_torreFinal_Medio():
     disco_retirar = TorreMedio.pop()
     TorreFinal.items.append(disco_retirar)
-    
+
 
 def Llenar_TorreMedioconInicial():
     disco_retirar = TorreInicial.pop()
     TorreMedio.items.append(disco_retirar)
 
+
 def Llenar_TorreMedioconFinal():
     disco_retirar = TorreFinal.pop()
     TorreMedio.items.append(disco_retirar)
+
 
 def Llenar_TorreInicialconFinal():
     disco_retirar = TorreFinal.pop()
     TorreInicial.items.append(disco_retirar)
 
+
 def Llenar_TorreInicialconMedio():
     disco_retirar = TorreMedio.pop()
-    TorreInicial.items.append(disco_retirar) 
-    
+    TorreInicial.items.append(disco_retirar)
 
 
-if cantidad == 3: 
+if cantidad == 3:
     Llenar_torreInicial()
-    print("Las torre contienen los siguientes discos: ", TorreInicial.items, TorreMedio.items, TorreFinal.items)
+    print("")
+    print("\n{:3}{:<25} {:<25}{:<25} ".format("  ", "TORRE INICIAL", "TORRE DEL MEDIO", "TORRE FINAL"))
+    print("{:3}{:<25} {:<25}{:<25} ".format("  ", str(TorreInicial.items), str(TorreMedio.items), str(TorreFinal.items)))
+
+    #print("Las torre contienen los siguientes discos: ", TorreInicial.items, TorreMedio.items, TorreFinal.items)
     Llenar_torreFinalconInicial()
-    print("Las torre contienen los siguientes discos: ", TorreInicial.items, TorreMedio.items, TorreFinal.items)
+    print("{:3}{:<25} {:<25}{:<25} ".format("  ", str(TorreInicial.items), str(TorreMedio.items), str(TorreFinal.items)))
+
+    #print("Las torre contienen los siguientes discos: ", TorreInicial.items, TorreMedio.items, TorreFinal.items)
     Llenar_TorreMedioconInicial()
-    print("Las torre contienen los siguientes discos: ", TorreInicial.items, TorreMedio.items, TorreFinal.items)
+    print("{:3}{:<25} {:<25}{:<25} ".format("  ", str(TorreInicial.items), str(TorreMedio.items), str(TorreFinal.items)))
+
+    #print("Las torre contienen los siguientes discos: ", TorreInicial.items, TorreMedio.items, TorreFinal.items)
     Llenar_TorreMedioconFinal()
-    print("Las torre contienen los siguientes discos: ", TorreInicial.items, TorreMedio.items, TorreFinal.items)
+    print("{:3}{:<25} {:<25}{:<25} ".format("  ", str(TorreInicial.items), str(TorreMedio.items), str(TorreFinal.items)))
+
+    #print("Las torre contienen los siguientes discos: ", TorreInicial.items, TorreMedio.items, TorreFinal.items)
     Llenar_torreFinalconInicial()
-    print("Las torre contienen los siguientes discos: ", TorreInicial.items, TorreMedio.items, TorreFinal.items)
+    print("{:3}{:<25} {:<25}{:<25} ".format("  ", str(TorreInicial.items), str(TorreMedio.items), str(TorreFinal.items)))
+
+    #print("Las torre contienen los siguientes discos: ", TorreInicial.items, TorreMedio.items, TorreFinal.items)
     Llenar_TorreInicialconMedio()
-    print("Las torre contienen los siguientes discos: ", TorreInicial.items, TorreMedio.items, TorreFinal.items)
+    print("{:3}{:<25} {:<25}{:<25} ".format("  ", str(TorreInicial.items), str(TorreMedio.items), str(TorreFinal.items)))
+
+    #print("Las torre contienen los siguientes discos: ", TorreInicial.items, TorreMedio.items, TorreFinal.items)
